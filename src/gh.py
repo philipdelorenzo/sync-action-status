77ad1ea97a036dcbc50b04fd4fdd8813916c7e00
Philip De Lorenzo <philip.delorenzo@gmail.com>
2024-09-09 22:25:35 -0700

    chore: add worspace context

---
+++ src/gh.py
@@ -0,0 +1,28 @@
+import os
+from github import Github
+
+# Authentication is defined via github.Auth
+from github import Auth
+
+# using an access token
+def auth(token: str) -> Auth.Token:
+    return Auth.Token(token)
+
+# Public Web Github
+def github(auth: Auth.Token) -> Github:
+    return Github(auth=auth)
+
+# Github Enterprise with custom hostname
+#g = Github(base_url="https://{hostname}/api/v3", auth=auth)
+
+# Then play with your Github objects:
+def get_repos(conn: Github) -> list:
+    repos = []
+    for repo in conn.get_user().get_repos():
+        repos.append(repo)
+    
+    return repos
+
+# To close connections after use
+def close(conn: Github) -> None:
+    conn.close()
