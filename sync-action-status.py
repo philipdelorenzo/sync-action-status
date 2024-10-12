e3b7ed9d6cd462be7fc109e2a1ba4d318fb58209
Philip De Lorenzo <philip.delorenzo@gmail.com>
2024-10-12 13:25:34 -0700

    chore: add small changes to Makefile

--- sync-action-status.py
+++ sync-action-status.py
@@ -104,76 +104,72 @@ if __name__ == "__main__":
     # If the is_org flag is set to True, then we want to use the org argument
     repo_dispatch = get_repository_dispatch(args=args)
 
-    if args.is_org:
-        ic(f"Organization: {args.org}")
+    ic(f"Actor\Org: {gh_actor_org}")
 
-    else:
-        ic(f"Actor: {gh_actor_org}")
+    # Let's get our auth setup for the GitHub API
+    auth = auth(gh_token=_gh_token)
+    api = github(auth=auth)  # Connect to the GitHub API
 
-        # Let's get our auth setup for the GitHub API
-        auth = auth(gh_token=_gh_token)
-        api = github(auth=auth)  # Connect to the GitHub API
+    workflow_data = get_workflow_data(repo=repo_dispatch) # Let's get a list of the workflows in the repos
+    ic(f"Workflow Data: {workflow_data}")
+    
+    event_type_data = get_event_type_list_for_workflows(repo=repo_dispatch) # Let's get a list of the event types for the workflows
+    ic(f"Event Type Data: {event_type_data}")
 
-        workflow_data = get_workflow_data(repo=repo_dispatch) # Let's get a list of the workflows in the repos
-        ic(f"Workflow Data: {workflow_data}")
-        
-        event_type_data = get_event_type_list_for_workflows(repo=repo_dispatch) # Let's get a list of the event types for the workflows
-        ic(f"Event Type Data: {event_type_data}")
-
-        # This will return a dictionary of the event types for the workflows
-        filtered_events = {} # Let's create an empty dictionary to store the filtered events
-        for _name, _event in event_type_data.items():
-            ic(f"Event Type Name: {_name} - Event Type Data: {_event}")
-            if len(_event) == 1:
-                if args.event_type in _event:
-                    print(f"Event Type: {args.event_type} found in {_event}")
-                    filtered_events[_name] = _event
-                else:
-                    print(f"Event Type: {args.event_type} not found in {_event}")
-                    exit(5)
-
-            elif len(_event) > 1:
-                for i in _event:
-                    ic(f"Event Type {args.event_type} --> {_event}")
-                    print("Development work needed here.")
-                    exit(0)
-
+    # This will return a dictionary of the event types for the workflows
+    filtered_events = {} # Let's create an empty dictionary to store the filtered events
+    for _name, _event in event_type_data.items():
+        ic(f"Event Type Name: {_name} - Event Type Data: {_event}")
+        if len(_event) == 1:
+            if args.event_type in _event:
+                print(f"Event Type: {args.event_type} found in {_event}")
+                filtered_events[_name] = _event
             else:
                 print(f"Event Type: {args.event_type} not found in {_event}")
                 exit(5)
 
-        ic(f"Filtered Events: {filtered_events}")
-
-        if len(filtered_events) > 1:
-            print(f"Found multiple workflows for the event type {args.event_type}")
-            sys.exit(5)
-
-        # This currently assumes there is ONLY 1 workflow for the event type
-        # This is subject to change as we mature and add more workflows - new logic will be needed here to accomadate
-        if len(filtered_events) == 1:
-            workflow_name = list(filtered_events.keys())[0] # This will be used as the key to find the id number of the workflow
-            workflow_id = get_workflow_id(_name=workflow_name, repo=repo_dispatch) # This will return the id of the workflow passed in as an argument
-            current_running_jobs = current_running_job_list(_name=workflow_name, repo=repo_dispatch)
-            filtered_jobs = filter_job_list(current_running_jobs)
-            _workflow_job_id = filtered_jobs['databaseId']
-
-            ic(f"Worflow Data: {workflow_data}")
-            ic(f"Event Type Data: {event_type_data}")
-
-            ic(f"Current Running Jobs: {current_running_jobs}")
-            ic(f"Filtered Jobs: {filtered_jobs}")
-
-            # Let's print the workflow name and id to the console
-            print(f"Workflow Name: {workflow_name}")
-            print(f"Workflow ID: {workflow_id}")
-            print(f"Workflow Run ID: {_workflow_job_id}")
-
-            # Let's follow the logs of the workflow and exit status
-            follow_workflow_job(workflow_job_id=_workflow_job_id, interval=args.interval, repo=repo_dispatch)
+        elif len(_event) > 1:
+            for i in _event:
+                ic(f"Event Type {args.event_type} --> {_event}")
+                print("Development work needed here.")
+                exit(0)
 
         else:
-            print(f"Multiple event types {args.event_type} found -- SRE Code changes needed to facilitate.")
-            sys.exit(5)
+            print(f"Event Type: {args.event_type} not found in {_event}")
+            exit(5)
 
-        # Close the connection
-        api.close()
+    ic(f"Filtered Events: {filtered_events}")
+
+    if len(filtered_events) > 1:
+        print(f"Found multiple workflows for the event type {args.event_type}")
+        sys.exit(5)
+
+    # This currently assumes there is ONLY 1 workflow for the event type
+    # This is subject to change as we mature and add more workflows - new logic will be needed here to accomadate
+    if len(filtered_events) == 1:
+        workflow_name = list(filtered_events.keys())[0] # This will be used as the key to find the id number of the workflow
+        workflow_id = get_workflow_id(_name=workflow_name, repo=repo_dispatch) # This will return the id of the workflow passed in as an argument
+        current_running_jobs = current_running_job_list(_name=workflow_name, repo=repo_dispatch)
+        filtered_jobs = filter_job_list(current_running_jobs)
+        _workflow_job_id = filtered_jobs['databaseId']
+
+        ic(f"Worflow Data: {workflow_data}")
+        ic(f"Event Type Data: {event_type_data}")
+
+        ic(f"Current Running Jobs: {current_running_jobs}")
+        ic(f"Filtered Jobs: {filtered_jobs}")
+
+        # Let's print the workflow name and id to the console
+        print(f"Workflow Name: {workflow_name}")
+        print(f"Workflow ID: {workflow_id}")
+        print(f"Workflow Run ID: {_workflow_job_id}")
+
+        # Let's follow the logs of the workflow and exit status
+        follow_workflow_job(workflow_job_id=_workflow_job_id, interval=args.interval, repo=repo_dispatch)
+
+    else:
+        print(f"Multiple event types {args.event_type} found -- SRE Code changes needed to facilitate.")
+        sys.exit(5)
+
+    # Close the connection
+    api.close()
