90599a70810e649403147d39db9791b51110a142
Philip De Lorenzo <philip.delorenzo@gmail.com>
2024-10-12 12:08:34 -0700

    chore: add small changes to Makefile

--- src/checks.py
+++ src/checks.py
@@ -25,11 +25,3 @@ def prerequisites(args: argparse.ArgumentParser.parse_args) -> None:
     """This function checks to ensure that all prerequisites are met before running the script."""
     if (not args.repo) or (args.repo == ""):
         raise RepoError("The --repo argument is required (this is the repository that houses the receiver action).")
-
-    if args.is_org:
-        if (not args.org) or (args.org == ""):
-            raise RepoError("The --org argument is required if this is an organization (this is the organization that houses the receiver action).")
-
-    if (os.getenv('GH_ACTOR') is None) or (os.getenv('GH_ACTOR') == ""):
-        raise GithubActorError("Cannot retrieve the Github Actor - This is needed for the action to run.")
-    
